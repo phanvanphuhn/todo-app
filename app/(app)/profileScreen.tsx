a2d9d40001d7a6cb576e672a5e3a7d9db0979dab
Jeff Phan <72235802+phanvanphuhn@users.noreply.github.com>
2025-09-02 22:45:42 +0700

    show username at profile screen

--- app/(app)/profileScreen.tsx
+++ app/(app)/profileScreen.tsx
@@ -1,9 +1,16 @@
 import { View, Text, StyleSheet } from "react-native";
+import { useAuthStore } from "../../stores/authStore";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function ProfileScreen() {
+  const { currentUser } = useAuthStore();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Profile Screen</Text>
+      <Text style={styles.title}>{currentUser?.username}</Text>
+      <View style={styles.profileContainer}>
+        <MaterialCommunityIcons name="human-male" size={48} color="black" />
+      </View>
     </View>
   );
 }
@@ -11,12 +18,23 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    padding: 16,
     alignItems: "center",
     backgroundColor: "#fff",
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
+  },
+  profileContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 8,
+    padding: 48,
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 99,
+    backgroundColor: "#007AFF",
   },
 });
